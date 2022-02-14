1c947c808544fd829ff168c09ad364bb1c5f1caa
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2022-02-14 21:14:00 +0600

    Spotify UI

---
+++ lib/models/song_model.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/data/data.dart';
+
+class Song {
+  final String id;
+  final String title;
+  final String artist;
+  final String album;
+  final String duration;
+
+  const Song({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.album,
+    required this.duration,
+  });
+}
